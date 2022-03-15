f6cccf92d093b44a00d3ed843ee7bb00dcff6c41
WhiteOokami <alanyi2006@gmail.com>
2022-03-15 23:35:08 +0800

    Imported Assets and added lighting
    Assets:
    - Epic Toon Fx (effects)
    - Pro Camera 2D (camera)
    - DoTween Pro (tweening animations)
    - All In 1 Sprite Shader (effects)
    - Smart Lighting 2D (lighting)

---
+++ Assets/code/gameplay/electricFieldLines.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class electricFieldLines : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // calculate direction & magniute of electric field at a given point
+    (int,int) E()
+    {
+        int magnitude = 0;
+        int dir = 0;
+        return (magnitude,dir);
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
