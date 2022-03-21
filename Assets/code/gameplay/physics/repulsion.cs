aaeeca78e81d6c0a4d8c63794d3c013c9af9b66b
Ray-Catty <96339178+Ray-Catty@users.noreply.github.com>
2022-03-21 14:27:54 +0800

    Compatibility fix

--- Assets/code/gameplay/physics/repulsion.cs
+++ Assets/code/gameplay/physics/repulsion.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class repulsion : MonoBehaviour {
     readonly float k = 8987551792f;
     public float qt = 1f;
-    GameObject charge;
+    //GameObject charge;
     GameObject[] efield;
     public bool started = false;
 
     void Start() {
-        charge = GameObject.FindGameObjectWithTag("Player");
+        //charge = GameObject.FindGameObjectWithTag("Player");
     }
 
     public void startPhysics()
@@ -33,9 +33,9 @@ public class repulsion : MonoBehaviour {
             foreach (GameObject e in efield)
             {
                 float q2 = e.GetComponent<chargepos>().chargeColomb;
-                float d = Vector2.Distance(charge.transform.position, e.transform.position);
+                float d = Vector2.Distance(transform.position, e.transform.position);
 
-                charge.GetComponent<Rigidbody2D>().AddForce((e.transform.position - charge.transform.position).normalized * (-k * (qt * q2) / (d * d)));
+                GetComponent<Rigidbody2D>().AddForce((e.transform.position - transform.position).normalized * (-k * (qt * q2) / (d * d)));
             }
         }
 
