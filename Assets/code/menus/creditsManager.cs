98ea66d16a1e73c79055ebe577f36cdecd3f819e
Ray-Catty <96339178+Ray-Catty@users.noreply.github.com>
2022-04-22 14:48:38 +0800

    credits music

--- Assets/code/menus/creditsManager.cs
+++ Assets/code/menus/creditsManager.cs
@@ -6,13 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class creditsManager : MonoBehaviour
 {
+    public AudioSource musicPlayer;
     public GameObject scrollBar;
     private bool escape;
 
+    void Awake() {
+        musicPlayer.Play();
+    }
     void FixedUpdate()
     {
         if (scrollBar.GetComponent<Scrollbar>().value > 0)
-            scrollBar.GetComponent<Scrollbar>().value -= 0.001f;
+            scrollBar.GetComponent<Scrollbar>().value -= 0.0008f;
     }
 
     public void escapeButton()
