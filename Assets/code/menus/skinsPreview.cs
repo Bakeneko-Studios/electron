7f45487ea98ba9689637860b9cf1c817851429a2
Ray-Catty <96339178+Ray-Catty@users.noreply.github.com>
2022-04-27 08:58:44 +0800

    better skins preview

--- Assets/code/menus/skinsPreview.cs
+++ Assets/code/menus/skinsPreview.cs
@@ -23,23 +23,27 @@ public class skinsPreview : MonoBehaviour
         transform.Find(skinsList[skinIndex]).gameObject.SetActive(true);                
     }
 
-    public int steps = 100;
     private bool offSet;
-
+    public float velocity;
     void FixedUpdate()
     {
         if (skinIndex == slot.transform.parent.gameObject.GetComponent<swipeManager>().selectedPanel)
         {
             transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(40f,40f),0.1f);
+            GetComponent<Rigidbody2D>().simulated = true;
             if (!offSet) {
                 transform.localPosition = new Vector3(50f,0,0);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0,velocity);
                 offSet = true;
             }
-            transform.RotateAround(slot.transform.position, Vector3.forward, 1f);
+
+            float r = Vector3.Distance(transform.position,slot.transform.position);
+            GetComponent<Rigidbody2D>().AddForce((slot.transform.position - transform.position).normalized * (velocity * velocity / r));
         }
         else {
             transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(100f,100f),0.1f);
             transform.localPosition = new Vector3(0,0,0);
+            GetComponent<Rigidbody2D>().simulated = false;
             offSet = false;
         }
     }
