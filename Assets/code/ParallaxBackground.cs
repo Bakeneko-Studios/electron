bd80146ecb5c1bcfc29c2aa924c7fafbe00405ab
a-piece-of-wood <dwasebailey@outlook.com>
2023-02-04 21:46:40 +0800

    background fix

--- Assets/code/ParallaxBackground.cs
+++ Assets/code/ParallaxBackground.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    private Transform backgroundTransform;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        backgroundTransform = gameObject.GetComponent<Transform>();
-    }
-
-    // Update is called once per frame
+    public float backgroundSpeed;
     void Update()
     {
-        backgroundTransform.position += new Vector3(1.6f, 0f, 0f) * Time.deltaTime;
-        if (backgroundTransform.position.x >= 17.1f)
-        {
-            backgroundTransform.position = new Vector3(-1.76f, -0.25f, 20f);
-        }
+        transform.position += new Vector3(backgroundSpeed,0,0) * Time.deltaTime;
+        if (transform.position.x >= 40f)
+            transform.position = Vector3.zero;
     }
 }
